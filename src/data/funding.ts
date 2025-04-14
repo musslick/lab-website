54f7c246162b8bd0e00285412e2e3d5b2c3c064a
Sebastian Musslick <sebastian@musslick.de>
2025-04-14 22:59:42 +0200

    added funding

--- src/data/funding.ts
+++ src/data/funding.ts
@@ -9,21 +9,37 @@ export interface FundingSource {
 }
 
 export const fundingSources: FundingSource[] = [
-  {
-    "id": "nsf-grant",
-    "name": "National Science",
-    "url": "https://www.nsf.gov",
-    "grantNumber": "NSF-1234567"
-  },
-  {
-    "id": "nih-grant",
-    "name": "National Institutes of Health",
-    "url": "https://www.nih.gov",
-    "grantNumber": "NIH-R01-AB123456"
-  },
-  {
-    "id": "research-foundation",
-    "name": "Science Research Foundation",
-    "url": "https://www.example.org/srf"
-  }
+    {
+      "id": "nsf-grant",
+      "name": "National Science Foundation",
+      "url": "https://www.nsf.gov",
+      "logo": "https://upload.wikimedia.org/wikipedia/commons/7/7e/NSF_logo.png",
+      "grantNumber": "NSF-2318549",
+      "amount": "USD 361,441",
+      "duration": "2023–2026"
+    },
+    {
+      "id": "nih-grant",
+      "name": "German Research Foundation",
+      "url": "https://www.dfg.de/",
+      "logo": "https://www.research-in-germany.org/dam/jcr:ff526773-07f4-444e-a634-d991bf8ad0e3/DFG%20logo.png",
+      "grantNumber": "NIH-R01-AB123456",
+      "amount": "EUR 270,483",
+      "duration": "2025–2028"
+    },
+    {
+      "id": "research-foundation",
+      "name": "Lower Saxony Ministry of Science and Culture",
+      "url": "https://www.mwk.niedersachsen.de/startseite/",
+      "logo": "https://www.niedersachsen.de/assets/image/736/17813",
+      "duration": "2025"
+    },
+    {
+      "id": "funding-1744664274715",
+      "name": "Schmidt Science Fellows",
+      "url": "https://schmidtsciencefellows.org/",
+      "logo": "https://schmidtsciencefellows.org/wp-content/uploads/ssf-logomark.png",
+      "amount": "USD 4000",
+      "duration": "2024-2025"
+    }
 ];
